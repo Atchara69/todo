544861004be8cd6b7c39b0231c467cf8b7cc160c
Atchara  Maneechot <60143246@g.cmru.ac.th>
2020-07-19 14:40:41 +0700

    ggggg

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,34 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return view('home');
+    public function index()
+    {
+        $posts = Post::paginate(5);
+        $data = [
+            'posts' => $posts
+        ];
+        return view('home', $data);
     }
-    public function show($id){
-
+    public function create()
+    {
+        $categories = Category::all();
+        $data = [
+            'categories' => $categories
+        ];
+        return view('create', $data);
     }
-    public function create(){
-
+    public function login()
+    {
+        return view('login');
     }
-    public function store(){
+    public function store(Request $request)
+    {
+        $detail = $request->input('detail');
+        $category_id = $request->input('category_id');
+        $post = new Post();
+        $post->user_id = 1;
+        $post->status = 0;
+        $post->detail = $detail;
+        $post->category_id = $category_id;
+        $post->save();
 
+        return redirect("/");
     }
-    public function update($id){
 
-    }
-    public function delete($id){
 
-    }
-    public function about(){
-        return view('about');
 
-    }
-    public function contact(){
-
-        return view('contact');
+    public function delete($id)
+    {
+        if ($id == "") {
+            return redirect('/');
+        }
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/');
     }
 }
